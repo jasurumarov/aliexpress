d608a0e9b2efbc03832151a35847703e5de28e71
jasurumarov <jasurumarov199@gmail.com>
2024-05-23 12:41:51 +0500

    finish

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,13 @@ import { useFetch } from '../../hooks/useFetch'
 
 // Components
 import Product from '../../components/product/Product'
+import Hero from '../../components/hero/Hero'
 
 const Home = () => {
   const {data, loading} = useFetch('/products') 
   return (
     <div className='home-page'>
+        <Hero/>
         <Product data={data?.data?.products} loading={loading}/>
     </div>
   )
